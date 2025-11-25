358aadd0e4412ce11ec007e5af87883428659c67
zyd-ustc <131000367+zyd-ustc@users.noreply.github.com>
2025-11-25 18:45:20 +0800

    Update sdxl_clip.py

--- comfy/sdxl_clip.py
+++ comfy/sdxl_clip.py
@@ -4,14 +4,14 @@ import mindspore
 from mindspore import mint
 
 class SDXLClipG(sd1_clip.SDClipModel):
-    def __init__(self, device=None, max_length=77, freeze=True, layer="penultimate", layer_idx=None, dtype=None, model_options={}):
+    def __init__(self, max_length=77, freeze=True, layer="penultimate", layer_idx=None, dtype=None, model_options={}):
         if layer == "penultimate":
             layer="hidden"
             layer_idx=-2
 
         textmodel_json_config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "clip_config_bigg.json")
         model_options = {**model_options, "model_name": "clip_g"}
-        super().__init__(device=None, freeze=freeze, layer=layer, layer_idx=layer_idx, textmodel_json_config=textmodel_json_config, dtype=dtype,
+        super().__init__(freeze=freeze, layer=layer, layer_idx=layer_idx, textmodel_json_config=textmodel_json_config, dtype=dtype,
                          special_tokens={"start": 49406, "end": 49407, "pad": 0}, layer_norm_hidden_state=False, return_projected_pooled=True, model_options=model_options)
 
     def load_sd(self, sd):
