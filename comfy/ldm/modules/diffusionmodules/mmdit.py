4b388dd3a67e128c5d527ead9dd61c811f4aa609
zyd-ustc <131000367+zyd-ustc@users.noreply.github.com>
2025-11-30 17:26:54 +0800

    Update mmdit.py

--- comfy/ldm/modules/diffusionmodules/mmdit.py
+++ comfy/ldm/modules/diffusionmodules/mmdit.py
@@ -526,8 +526,9 @@ class DismantledBlock(mint.nn.Cell):
     def post_attention(self, attn, x, gate_msa, shift_mlp, scale_mlp, gate_mlp):
         assert not self.pre_only
         x = x + gate_msa.unsqueeze(1) * self.attn.post_attention(attn)
+        tmp = self.norm2(x.to(mindspor.bfloat16)
         x = x + gate_mlp.unsqueeze(1) * self.mlp(
-            modulate(self.norm2(x), shift_mlp, scale_mlp)
+            modulate(tmp.to(x.dtype), shift_mlp, scale_mlp)
         )
         return x
 
@@ -561,8 +562,9 @@ class DismantledBlock(mint.nn.Cell):
         attn1 = self.attn.post_attention(attn)
         attn2 = self.attn2.post_attention(attn2)
         x = gate_cat(x, gate_msa, gate_msa2, attn1, attn2)
+        tmp = self.norm2(x.to(mindspor.bfloat16)
         x = x + gate_mlp.unsqueeze(1) * self.mlp(
-            modulate(self.norm2(x), shift_mlp, scale_mlp)
+            modulate(tmp.to(x.dtype), shift_mlp, scale_mlp)
         )
         return x
 
@@ -726,7 +728,8 @@ class ContextProcessorBlock(mint.nn.Cell):
 
     def construct(self, x):
         x += self.attn(self.norm1(x))
-        x += self.mlp(self.norm2(x))
+        tmp = self.norm2(x.to(bfloat16))
+        x += self.mlp(tmp.to(x.dtype))
         return x
 
 class ContextProcessor(mint.nn.Cell):
